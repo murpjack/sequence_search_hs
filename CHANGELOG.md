da2550906eecaec087513e5394e00cc5f0217973
Jack <jackmurphy756@gmail.com>
2023-06-12 20:43:48 +0100

    Fix - now handles cases where many patterns begin the same.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.0.0.1 -- 2023-06-12
 
 - Algorithm finds matches. But can not handle cases where multiple patterns begin the same
+
+## 0.0.0.2 -- 2023-06-12
+
+- Fix - now handles cases where many patterns begin the same.
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Data.List as List
-import qualified Data.Tuple as Tuple
+import qualified Data.Tuple.Extra as Tuple
 import qualified System.Environment as SysEnv
 import qualified System.IO as SysIO
 
@@ -18,29 +18,43 @@ toLines filename = List.lines <$> SysIO.readFile filename
 wrapExpr :: String -> String
 wrapExpr e = "{" ++ e ++ "}"
 
--- Can find matches. Can not handle where multiple patterns begin the same
+-- Finds matches; and when many patterns begin the same.
+-- TODO: Can not match where uppercase chars. Should it?
+-- TODO: Can not match where punctuation exists
 writeLn :: [String] -> String -> String
 writeLn ptns =
-  Tuple.fst
+  Tuple.fst3
     . List.foldl
-      ( \(ln, match) t ->
-          if List.null match
-            then case List.find (\ptn -> List.head ptn == t) ptnLs of
-              Just ptn ->
-                if List.length ptn > 1
-                  then (ln, List.head ptn : match)
-                  else (ln ++ " " ++ wrapExpr t, [])
-              Nothing ->
-                (ln ++ " " ++ t, [])
-            else case List.find (\ptn -> (match ++ [t]) `isPrefixOf` ptn) ptnLs of
-              Just ptn ->
-                if List.length ptn > List.length (match ++ [t])
-                  then (ln, match ++ [t])
-                  else (ln ++ " " ++ wrapExpr (List.unwords ptn), [])
-              Nothing -> (ln ++ " " ++ wrapExpr (List.unwords match) ++ " " ++ t, [])
+      ( \(ln, cache, partMatches) t ->
+          if List.null cache
+            then case List.findIndices (\ptn -> List.head ptn == t) ptnLs of
+              [] -> (ln ++ " " ++ t, [], [])
+              [n] ->
+                let ptn = ptnLs !! n
+                 in if List.length ptn > 1
+                      then (ln, [List.head ptn], [ptnLs !! n])
+                      else (ln ++ " " ++ wrapExpr t, [], [])
+              ns ->
+                let ptn = ptnLs !! List.head ns
+                 in (ln, [List.head ptn], findMatches ptnLs ns)
+            else case List.findIndices (\ptn -> (cache ++ [t]) `isPrefixOf` ptn) partMatches of
+              [] -> (ln ++ " " ++ wrapExpr (List.unwords cache) ++ " " ++ t, [], [])
+              [n] ->
+                let ptn = partMatches !! n
+                 in if List.length ptn > List.length (cache ++ [t])
+                      then (ln, cache ++ [t], partMatches)
+                      else (ln ++ " " ++ wrapExpr (List.unwords ptn), [], [])
+              ns ->
+                let fstMatch = partMatches !! List.head ns
+
+                    ptnPrfx = List.take (List.length cache) fstMatch
+                 in (ln, ptnPrfx ++ [t], findMatches partMatches ns)
       )
-      ("", [])
+      ("", [], [])
     . List.words
   where
     ptnLs :: [[String]]
     ptnLs = List.map List.words ptns
+
+findMatches :: [[String]] -> [Int] -> [[String]]
+findMatches ptns = List.map (ptns !!)
